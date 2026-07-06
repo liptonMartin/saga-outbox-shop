cd102b625db9996a09b2b424edd6bf6160cb0172
lipton <liptonmartin135@gmail.com>
2026-07-06 19:44:46 +0300

    user_service: migration: add column refresh_token

---
+++ services/user_service/migrations/20260706_02_6psjG-add-column-token-in-users-table.py
@@ -0,0 +1,11 @@
+"""
+add column token in users table
+"""
+
+from yoyo import step
+
+__depends__ = {'20260706_01_PtF6B-initial-users-table'}
+
+steps = [
+    step("ALTER TABLE users ADD refresh_token VARCHAR(256);")
+]
